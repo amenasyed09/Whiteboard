9670692980be1b180d7c129e6ff3f53806f3012c
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2025-12-24 12:33:18 +0530

    CHANGES INapi

--- Frontend/src/connection/api.js
+++ Frontend/src/connection/api.js
@@ -3,8 +3,9 @@ import axios from "axios";
 let accessToken = localStorage.getItem("accessToken");
 let refreshToken = localStorage.getItem("refreshToken");
 
+const API_URL = import.meta.env.VITE_API_URL || "https://whiteboard-backend-1els.onrender.com/api";
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:API_URL,
 });
 
 api.interceptors.request.use((config) => {
--- Frontend/src/connection/socket.js
+++ Frontend/src/connection/socket.js
@@ -1,10 +1,12 @@
 import { io } from "socket.io-client";
 
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://whiteboard-backend-1els.onrender.com";
 export const connectSocket = () => {
   const token = localStorage.getItem("accessToken");
 
-  return io("http://localhost:5000", {
+  return io(SOCKET_URL, {
     auth: { token },
     autoConnect: true,
+      transports: ["websocket", "polling"],
   });
 };
